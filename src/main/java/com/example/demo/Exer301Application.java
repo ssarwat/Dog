a6cb612f559a78e7b6af7306f364732b5bb4ec8e
ssarwat <sumaiyah_sarwat@yahoo.com>
2019-08-20 14:04:28 -0400

    dog application

---
+++ src/main/java/com/example/demo/Exer301Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exer301Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exer301Application.class, args);
+    }
+
+}
